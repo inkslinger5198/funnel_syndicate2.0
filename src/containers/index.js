a26553ad0d41ede0a3a4eff9126f94738cbcc411
Pranavi <139257931+inkslinger5198@users.noreply.github.com>
2023-11-17 11:40:26 +0530

    Delete src directory

--- src/containers/index.js
+++
@@ -1,8 +0,0 @@
-export {default as Header} from './header/Header';
-export {default as About} from './about/About';
-export {default as Service} from './service/Service';
-export {default as Testimonial} from './testimonial/Testimonial';
-export {default as FAQ} from './faq/FAQ';
-export {default as Contact} from './contact/Contact';
-export {default as Founders} from './founders/Founders';
-export {default as Footer} from './footer/Footer';
